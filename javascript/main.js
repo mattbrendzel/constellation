cd7b3015f60005efc04946a822792491110287cb
Matt Brendzel <matt.brendzel@gmail.com>
2020-04-03 18:02:52 -0400

    Add JS code to hide and show menus
    - When a menu label is clicked, show the menu's dropdown (and hide any
      other dropdowns that may be visible).
    - When an item from the dropdown is clicked, update the text shown in
      the main part of the page.
    - When the mouse enters the article, hide all dropdowns.

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+const resetMenuSelection = function () {
+  document.querySelectorAll('.menu-item').forEach(element => {
+    element.classList.remove('selected');
+  });
+};
+
+const selectNavMenuItemLabel = function (e) {
+  this.parentNode.classList.add('selected');
+};
+
+const selectMenuDropdownItem = function (e) {
+  document.querySelector('#content').textContent = "TEST";
+  resetMenuSelection();
+}
+
+window.onload = function() {
+  document.querySelectorAll('.menu-item').forEach(element => {
+    element.addEventListener('mouseenter', resetMenuSelection);
+  })
+  document.querySelectorAll('.menu-item-label').forEach(element => {
+    element.addEventListener('click', selectNavMenuItemLabel);
+  });
+  document.querySelectorAll('.menu-item-dropdown li').forEach(element => {
+    element.addEventListener('click', selectMenuDropdownItem);
+  })
+
+  document.querySelector('article').addEventListener('mouseenter', resetMenuSelection);
+};
